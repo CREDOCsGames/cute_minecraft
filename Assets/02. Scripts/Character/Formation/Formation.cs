c00054ffa94d585c0b463bd8053122393f198f9c
1506022022 <88864717+1506022022@users.noreply.github.com>
2024-07-11 15:52:40 +0900

    feat(Role): 스크립트 추가
    - 지정된 포메이션을 따라다니는 추적 기능의 스크립트입니다.
    - 포메이션은 의지와 행동 모두 바뀔 가능성이 높다고 생각해서 분리하는 것에 집중했습니다.
    - BaseRole에서는 이벤트 기반으로 의사를 표출합니다.
    - Role에서는 이벤트에 행동을 주입합니다.

--- Assets/02. Scripts/Character/Formation/Formation.cs
+++ Assets/02. Scripts/Character/Formation/Formation.cs
@@ -1,105 +1,75 @@
-using System;
-using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine;
+using System;
 
 namespace PlatformGame.Character
 {
     [Serializable]
-    public class Formation : MonoBehaviour
+    public class Formation
     {
-        public Transform objectA; // A 오브젝트
-        public Transform ToB; // B 오브젝트
-        Vector3 OriginPos;
-        // 이동 분리
-        public float time = 0f;
-        public float Duration = 10f;
-        private Vector3 controlPoint; // 제어점
-        public AnimationCurve curve;
-        public UnityEvent OnFormationChanged;
-
-        bool mbFormationChange;
-        bool mbEnter;
-
-        void FixedUpdate()
+        Transform mTransform;
+        public Transform Transform
         {
-            if (Vector3.Distance(objectA.position, ToB.position) < 0.1f)
+            get => mTransform;
+            set
             {
-                if(mbEnter)
-                {
-                    return;
-                }
-                mbEnter = true;
+                mTransform = value;
+                OnChangeFormation();
+            }
+        }
+        public UnityAction Trace;
+        public Func<bool> IsStoped;
+        public Func<bool> IsReached;
+        public UnityEvent OnReachFormationEvent;
+        public UnityEvent OnChangeFormationEvent;
+        public UnityEvent OnStopMoveEvent;
+        public UnityEvent OnStartMoveEvent;
 
-                if (mbFormationChange)
-                {
-                    OnFormationChanged.Invoke();
-                    mbFormationChange = false;
-                }
-
-                time = Duration;
+        public void UpdateBehaviour()
+        {
+#if DEVELOPMENT
+            var isReached = IsReached();
+            var isStoped = IsStoped();
+#endif
+            if (IsReached() && !IsStoped())
+            {
+                OnReachFormation();
                 return;
             }
 
-            if (mbEnter)
+            if (IsStoped() && !IsReached())
             {
-                mbEnter = false;
-            }
-
-            if (time == Duration)
-            {
-                time = 0;
-                controlPoint = (objectA.position + ToB.position) / 2f;
-            }
-
-            time = Mathf.Clamp(time + Time.deltaTime, 0, Duration);
-            Vector3 bezierPosition = CalculateBezierPoint(objectA.position, controlPoint, ToB.position, (time * curve.Evaluate(time / Duration)) / Duration);
-            objectA.position = bezierPosition;
-        }
-
-        public void PomationByState(CharacterState state)
-        {
-            if (state is CharacterState.Jumping or CharacterState.Falling)
-            {
-                SwapPomation(new Vector3(0, -2, 0), 0.5f);
-            }
-            else
-            {
-                PomationOrigin();
+                OnMoveToFormation();
             }
         }
 
-        public void SwapPomation(Vector3 pos, float duration)
+        void OnReachFormation()
         {
-            ToB.localPosition = pos;
-            time = 0;
-            controlPoint = (objectA.position + ToB.position) / 2f;
-            Duration = duration;
-            mbFormationChange = true;
-        }
-        public void PomationOrigin()
-        {
-            SwapPomation(OriginPos, 3f);
+            OnStopMove();
+            OnReachFormationEvent.Invoke();
         }
 
-        // 베지어 곡선 계산 함수
-        private Vector3 CalculateBezierPoint(Vector3 p0, Vector3 p1, Vector3 p2, float t)
+        void OnChangeFormation()
         {
-            float u = 1 - t;
-            float tt = t * t;
-            float uu = u * u;
-
-            Vector3 point = uu * p0; // 시작점
-            point += 2 * u * t * p1; // 제어점
-            point += tt * p2; // 끝점
-
-            return point;
+            OnStopMove();
+            OnChangeFormationEvent.Invoke();
         }
 
-        void Awake()
+        void OnStopMove()
         {
-            OriginPos = transform.localPosition;
+            OnStopMoveEvent.Invoke();
         }
+
+        void OnStartMove()
+        {
+            OnStartMoveEvent.Invoke();
+        }
+
+        void OnMoveToFormation()
+        {
+            Trace.Invoke();
+            OnStartMove();
+        }
+
     }
-
 }
-
