9e069a0e9eb27068b943db8aaa34939e1140cd36
1506022022 <88864717+1506022022@users.noreply.github.com>
2024-07-16 16:22:01 +0900

    feat(Compartments): 구획 색칠 구현

---
+++ Assets/02. Scripts/Contents/Compartment/Compartments.cs
@@ -0,0 +1,116 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace PlatformGame.Contents.Compartment
+{
+    public enum Color
+    {
+        None, Yellow, Green, Blue, Orange, Red, White
+    }
+
+    public class Compartments : MonoBehaviour
+    {
+        public List<Transform> YellowColors;
+        public List<Transform> GreenColors;
+        public List<Transform> BlueColors;
+        public List<Transform> OrangeColors;
+        public List<Transform> RedColors;
+        public List<Transform> WhiteColors;
+        List<List<Transform>> Colors;
+        public Color SymbolColor;
+        Color PaintedColor;
+        UnityEngine.Color[] ColorArray =
+        {
+            UnityEngine.Color.magenta,
+            UnityEngine.Color.yellow,
+            UnityEngine.Color.green,
+            UnityEngine.Color.blue,
+            new UnityEngine.Color(255,165,0),
+            UnityEngine.Color.red,
+            UnityEngine.Color.white,
+        };
+        public UnityEvent ActivationEvent;
+        public UnityEvent DeactivationEvent;
+        public float ActivationDuration;
+        float mElapsedTime = -1f;
+        bool mbInfinitelyActive;
+        public bool InfinitelyActive
+        {
+            get => mbInfinitelyActive;
+            set => mbInfinitelyActive = value;
+
+        }
+        public bool IsActivation => 0 <= mElapsedTime;
+
+        public void ColorizeCompartments(Transform transform)
+        {
+            if (IsActivation)
+            {
+                return;
+            }
+            PaintedColor = FindColor(transform);
+            mbInfinitelyActive = SymbolColor == PaintedColor;
+            Activetion();
+        }
+
+        public void SetEffectColor(LineRenderer renderer)
+        {
+            renderer.startColor = ColorArray[(int)PaintedColor];
+            renderer.endColor = ColorArray[(int)PaintedColor];
+        }
+
+        Color FindColor(Transform transform)
+        {
+            var index = Colors.FindIndex(x => x.Contains(transform));
+            Debug.Assert(index >= 0, $"Not in the list : {transform.name}");
+            return (Color)index + 1;
+        }
+
+        public void Activetion()
+        {
+            mElapsedTime = 0f;
+            ActivationEvent.Invoke();
+        }
+
+        public void Deactivation()
+        {
+            DeactivationEvent.Invoke();
+        }
+
+        void Awake()
+        {
+            Colors = new()
+            {
+                YellowColors,
+                GreenColors,
+                BlueColors,
+                OrangeColors,
+                RedColors,
+                WhiteColors
+            };
+        }
+        void Update()
+        {
+            if (!IsActivation)
+            {
+                return;
+            }
+
+            if (mbInfinitelyActive)
+            {
+                return;
+            }
+
+            if (ActivationDuration <= mElapsedTime)
+            {
+                mElapsedTime = -1f;
+                Deactivation();
+                return;
+            }
+
+            mElapsedTime += Time.deltaTime;
+        }
+    }
+}
+
