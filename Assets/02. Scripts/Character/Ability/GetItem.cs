2cea09fb8ac4d2cdf83d25d51dcec679675470e7
1506022022 <88864717+1506022022@users.noreply.github.com>
2024-07-22 17:22:21 +0900

    chore(Crafting): 연출 작업
    - 시네머신을 사용한 크래프팅 제작 화면 연출 작업

--- Assets/02. Scripts/Character/Ability/GetItem.cs
+++ Assets/02. Scripts/Character/Ability/GetItem.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
@@ -6,19 +7,40 @@ namespace PlatformGame.Character.Combat
     public class GetItem : MonoBehaviour
     {
         public float Range;
-
+        Dictionary<Item, int> mInventory = new();
         public void Get()
         {
             var foundItems = Item.Instances.Where(x => Vector3.Distance(transform.position, x.transform.position) < Range)
                                            .Where(x => x.GetComponent<Character>().State != CharacterState.Die)
                                            .ToList();
 
-            foreach(var item in foundItems)
+            foreach (var item in foundItems)
             {
                 var character = item.GetComponent<Character>();
+                if (mInventory.ContainsKey(item))
+                {
+                    mInventory[item]++;
+                }
+                else
+                {
+                    mInventory.Add(item, 1);
+                }
+
                 PlatformGame.Character.Combat.Destroy.DestroyTo(character);
             }
 
+        }
+
+        public void Push(Crafting crafting)
+        {
+            foreach (var item in mInventory.ToList())
+            {
+                for (int i = 0; i < item.Value; i++)
+                {
+                    crafting.InputItem(item.Key);
+                }
+                mInventory.Remove(item.Key);
+            }
         }
     }
 
