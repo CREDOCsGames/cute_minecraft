4adb80261907dfb1caeb7ea4df27c54ed6c15893
1506022022 <88864717+1506022022@users.noreply.github.com>
2024-07-31 13:08:44 +0900

    feat(Sound): 사운드 매니저 추가
    - 사운드를 데이터로 관리하는 사운드 리스트
    - 필요한 상황에 사용할 수 있도록 도와주는 사운드 핸들러
    - 사운드 재생을 관리해주는 매니저

---
+++ Assets/02. Scripts/Sound/SoundList.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SoundList", menuName = "ScriptableObject/SoundList", order = int.MaxValue)]
+public class SoundList : ScriptableObject
+{
+    public List<AudioClip> m_AudioClipList;
+
+    private Dictionary<string, AudioClip> m_AudioClipDictionary;
+
+    public Dictionary<string, AudioClip> AudioClips
+    {
+        get
+        {
+            if (m_AudioClipDictionary == null || m_AudioClipList.Count != m_AudioClipDictionary.Count)
+                m_AudioClipDictionary = GetSoundList();
+
+            return m_AudioClipDictionary;
+        }
+    }
+
+    private Dictionary<string, AudioClip> GetSoundList()
+    {
+        Dictionary<string, AudioClip> audioDictionary = new Dictionary<string, AudioClip>();
+
+        foreach (AudioClip item in m_AudioClipList)
+        {
+            if (audioDictionary.ContainsKey(item.name))
+                continue;
+            audioDictionary.Add(item.name, item);
+        }
+
+        return audioDictionary;
+    }
+}
---
+++ Assets/02. Scripts/Sound/SoundManager.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    private static SoundManager m_instance;
+    public static SoundManager Instance
+    {
+        get
+        {
+            if (m_instance == null)
+            {
+                m_instance = FindObjectOfType<SoundManager>();
+            }
+            return m_instance;
+        }
+    }
+
+    public SoundList m_SoundList;
+
+    public AudioSource m_MusicChannel;
+
+    public int m_InitSoundChannelCount = 10;
+    private List<AudioSource> m_SoundChannels;
+
+    void Awake()
+    {
+        m_SoundChannels = new List<AudioSource>();
+
+        for (int i = 0; i < m_InitSoundChannelCount; i++)
+        {
+            m_SoundChannels.Add(gameObject.AddComponent<AudioSource>());
+        }
+    }
+
+    #region Sound
+    public float PlaySound(string clipName)
+    {
+        if (m_SoundList.AudioClips.TryGetValue(clipName, out AudioClip clip))
+        {
+            int channelIndex = GetEmptyChannelIndex(m_SoundChannels);
+
+            m_SoundChannels[channelIndex].clip = clip;
+            m_SoundChannels[channelIndex].Play();
+            return m_SoundChannels[channelIndex].clip.length;
+        }
+        else
+        {
+            Debug.LogError("audio clip " + clipName + " does not exist");
+            return 0f;
+        }
+    }
+
+    private int GetEmptyChannelIndex(List<AudioSource> soundChannels)
+    {
+        for (int i = 0; i < soundChannels.Count; i++)
+        {
+            if (soundChannels[i].isPlaying == false)
+                return i;
+        }
+        soundChannels.Add(this.gameObject.AddComponent<AudioSource>());
+        return soundChannels.Count - 1;
+    }
+
+    public void StopSound()
+    {
+        for (int i = 0; i < m_SoundChannels.Count; i++)
+        {
+            m_SoundChannels[i].Stop();
+        }
+    }
+
+    public void MuteSound()
+    {
+        for (int i = 0; i < m_SoundChannels.Count; i++)
+        {
+            m_SoundChannels[i].volume = 0f;
+        }
+    }
+
+    public void ListenSound()
+    {
+        for (int i = 0; i < m_SoundChannels.Count; i++)
+        {
+            m_SoundChannels[i].volume = 1f;
+        }
+    }
+    #endregion
+
+    #region Music
+    public float PlayMusic(string clipName)
+    {
+        if (m_MusicChannel.clip != null && m_MusicChannel.isPlaying && m_MusicChannel.clip.name.Equals(clipName))
+        {
+            return 0f;
+        }
+
+        if (m_SoundList.AudioClips.TryGetValue(clipName, out AudioClip clip))
+        {
+            m_MusicChannel.clip = clip;
+            m_MusicChannel.Play();
+            return m_MusicChannel.clip.length;
+        }
+        else
+        {
+            Debug.LogError("audio clip " + clipName + " does not exist");
+            return 0f;
+        }
+    }
+
+    public void StopMusic()
+    {
+        m_MusicChannel.Stop();
+    }
+
+    public void MuteMusic()
+    {
+        m_MusicChannel.volume = 0f;
+    }
+
+    public void ListenMusic()
+    {
+        m_MusicChannel.volume = 1f;
+    }
+    public void SetMusicVolum(float volume, float duration)
+    {
+        StartCoroutine(LerpVolume(m_MusicChannel, volume, duration));
+    }
+
+    private IEnumerator LerpVolume(AudioSource audioChannel, float targetVolume, float duration)
+    {
+        float beginVolume = audioChannel.volume;
+        float volumeOffset = targetVolume - beginVolume;
+        for (float time = 0; time < duration; time += Time.deltaTime)
+        {
+            audioChannel.volume = beginVolume + (volumeOffset * (time / duration));
+            yield return null;
+        }
+        audioChannel.volume = targetVolume;
+    }
+    #endregion
+
+    public void MuteAll()
+    {
+        MuteMusic();
+        MuteSound();
+    }
+
+    public void ListenAll()
+    {
+        ListenMusic();
+        ListenSound();
+    }
+}
