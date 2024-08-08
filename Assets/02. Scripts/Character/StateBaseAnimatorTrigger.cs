12953b8d01b257f91016e9ece178bcead09b28e4
1506022022 <88864717+1506022022@users.noreply.github.com>
2024-08-08 21:38:10 +0900

    chore(Level): 구획 I 프리펩 작업, 애니메이션, 위치 표시 수정

--- Assets/02. Scripts/Character/StateBaseAnimatorTrigger.cs
+++ Assets/02. Scripts/Character/StateBaseAnimatorTrigger.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace PlatformGame.Character
@@ -6,15 +7,21 @@ namespace PlatformGame.Character
     {
         [Header("References")]
         [SerializeField] Character mCharacter;
+        string[] mTriggers;
 
         void Awake()
         {
-            Debug.Assert(mCharacter != null,$"Not found Character : {name}");
+            Debug.Assert(mCharacter != null, $"Not found Character : {name}");
             mCharacter.OnChangedState.AddListener(SendTrigger);
+            mTriggers = Enum.GetNames(typeof(CharacterState));
         }
 
         void SendTrigger(CharacterState state)
         {
+            foreach (var trigger in mTriggers)
+            {
+                mCharacter.Animator.ResetTrigger(trigger);
+            }
             mCharacter.Animator.SetTrigger(state.ToString());
         }
 
