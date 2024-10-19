265ac62e17c2345021c5f4a9310507910a128a5f
HJSKOR <barma3678@gmail.com>
2024-10-19 21:36:11 +0900

    chore(레벨 작업):"인트로 10/19"

---
+++ Assets/02. Scripts/CameraShakeEffect.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShakeEffect : MonoBehaviour
+{
+    public float ShakeAmount;
+    float shakeTime;
+
+    Vector3 initPos;
+
+    public void VibeTime(float time)
+    {
+        shakeTime = time;
+    }
+
+
+    private void Start()
+    {
+        initPos = new Vector3(0f, 0f, -5f);
+    }
+
+    private void Update()
+    {
+        if(shakeTime>0)
+        {
+            transform.position = Random.insideUnitSphere * ShakeAmount + initPos;
+            shakeTime -= Time.deltaTime;
+        }
+
+        else
+        {
+            shakeTime = 0.0f;
+            transform.position = initPos;
+        }
+    }
+}
