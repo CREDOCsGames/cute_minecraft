a9b8bdb3a3a41a28c41a2b6e6b2d8175c12d7155
TimeFlies0823 <130940591+TimeFlies0823@users.noreply.github.com>
2025-01-22 20:22:11 +0900

    feat(Boss): 육면체 하강 수정

--- Assets/02. Scripts/Battle/WeightFall.cs
+++ Assets/02. Scripts/Battle/WeightFall.cs
@@ -6,82 +6,58 @@ public class WeightFall : MonoBehaviour, IInstance, IPuzzleInstance
 {
     public DataReader DataReader { get; private set; } = new FlowerReader();
     private int _flowerCount;
-    private ParticleSystem particle;
+    private bool _falling = false;
     [SerializeField] private float speed;
-    [SerializeField] private Vector3 offset;
-    [SerializeField] private Vector3 rotation;
-    [SerializeField] private Transform _cube;
-    [SerializeField] private GameObject _dust;
-    [SerializeField] private NoiseSettings noiseSettings;
     [SerializeField] private CinemachineVirtualCamera cinemachine;
+    [SerializeField] private ParticleSystem particle;
     [SerializeField] private Transform _bg;
     public void SetMediator(IMediatorInstance mediator)
     {
     }
     public void Init(CubePuzzleDataReader puzzleData)
     {
-        _cube = puzzleData.BaseTransform;
-        //camera Setting
-        GameObject camera = Instantiate(cinemachine.gameObject);
-        camera.SetActive(false);
-        CinemachineVirtualCamera virtualCamera = camera.GetComponent<CinemachineVirtualCamera>();
-        virtualCamera.LookAt = _cube;
-        camera.transform.position += offset;
-        camera.transform.rotation = Quaternion.Euler(rotation);
-
-        //effect Setting
-        float dust_Ypos = (_cube.localScale.y / 2) + 1;
-        Vector3 dustPos = new Vector3(0, -dust_Ypos, 0);
-
-        _dust = Instantiate(_dust);
-
-        _dust.transform.position = dustPos;
-        _dust.transform.rotation = Quaternion.Euler(new Vector3(90, 0, -50));
-        _dust.transform.localScale = new Vector3(0.3f, 0.8f, 0.5f);
-
-        particle = _dust.GetComponent<ParticleSystem>();
-
+        cinemachine.LookAt = puzzleData.BaseTransform;
     }
     public void InstreamData(byte[] data)
     {
-        if (FlowerReader.FLOWER_CREATE.Equals(data))
+        if (MonsterReader.BOSS_SPIT_OUT_SUCCESS.Equals(data)) 
         {
             _flowerCount++;
-            switch (_flowerCount)
+            if(_flowerCount > 19 && _flowerCount % 3 ==0)
             {
-                case 3: FallLevelFunc(); break;
-                case 6: FallLevelFunc(); break;
-                case 9: FallLevelFunc(); break;
-                case 12: FallLevelFunc(); break;
-                case 15: FallLevelFunc(); break;
-                case 18: FallLevelFunc(); break;
+                FallCube();
             }
         }
     }
-
-    private void FallLevelFunc()
+    private void Update()
     {
-        Vector3 pre_cube = _cube.position + Vector3.down;
-        Vector3 pre_dust = _dust.transform.position + Vector3.down;
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            FallCube();
+        }
+    }
+    private void FallCube()
+    {
+        Vector3 pre_bg = _bg.position + Vector3.up;
         cinemachine.gameObject.SetActive(true);
         particle.gameObject.SetActive(true);
         particle.Play();
-        StartCoroutine(FallLevel(pre_cube, pre_dust));
+        StartCoroutine(FallLevel(pre_bg));
     }
-    private IEnumerator FallLevel(Vector3 pre_cube, Vector3 pre_dust)
+    private IEnumerator FallLevel(Vector3 pre_bg)
     {
+        if (_falling) { yield break; }
+        _falling = true;
         while (true)
         {
-            if (Mathf.Abs(_cube.position.y - pre_cube.y) < 0.1f)
+            if (Mathf.Abs(_bg.position.y - pre_bg.y) < 0.1f)
             {
-                _cube.position = pre_cube;
-                _dust.transform.position = pre_dust;
                 cinemachine.gameObject.SetActive(false);
                 particle.gameObject.SetActive(false);
+                _falling = false;
                 break;
             }
-            _cube.position = Vector3.MoveTowards(_cube.position, pre_cube, speed);
-            _dust.transform.position = Vector3.MoveTowards(_dust.transform.position, pre_dust, speed);
+            _bg.position = Vector3.MoveTowards(_bg.position, pre_bg,speed);
             yield return null;
         }
         yield break;
