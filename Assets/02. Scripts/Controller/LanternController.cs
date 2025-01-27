63d7656770739039b7c82e2b608a0c4865011bdf
NewRound <86642886+NewRound@users.noreply.github.com>
2025-01-27 23:00:15 +0900

    feat(Lantern): 랜턴컨트롤러 추가 및 완성
    에셋 규격에 맞게 script 폴더에 완성본 추가.

---
+++ Assets/02. Scripts/Controller/LanternController.cs
@@ -0,0 +1,128 @@
+using Puzzle;
+using System.Collections;
+using UnityEngine;
+
+public class LanternController : MonoBehaviour, IInstance, IPuzzleInstance, IDestroyable
+{
+    public DataReader DataReader { get; private set; } = new SystemReader();
+
+    public float MoveTime;
+    public float LenternInterval;
+    public Vector3 LanternMoveHight;
+    public Transform Cube;
+
+    public Vector3 FrontSide;
+    public Vector3 LeftSide;
+    public Vector3 RightSide;
+    public Vector3 BackSide;
+    public bool IsDisapear { get; private set; } = false;
+
+    private CubePuzzleDataReader _castingPuzzleData;
+    private Vector3 _cubeCenter;
+    private byte _cubeWidth;
+
+    public void Init(CubePuzzleDataReader puzzleData)
+    {
+        this.gameObject.SetActive(true);
+        _castingPuzzleData = puzzleData;
+        _castingPuzzleData.OnRotatedStage += SetLenternPosition;
+        _cubeWidth = puzzleData.Width;
+        SetSpawnPoint(_castingPuzzleData);
+    }
+
+    public void Destroy()
+    {
+        _castingPuzzleData.OnRotatedStage -= SetLenternPosition;
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void Lentern_Active(Vector3 spwanPos)
+    {
+        StartCoroutine(MoveLantern(spwanPos));
+    }
+
+    private Vector3 SwitchPositionFromDirection(Vector3 direction)
+    {
+        if (direction == new Vector3(-1, 0, 0))
+        {
+            return LeftSide;
+        }
+        else if (direction == new Vector3(1, 0, 0))
+        {
+            return RightSide;
+        }
+        else if (direction == new Vector3(0, 0, -1))
+        {
+            return FrontSide;
+        }
+        else if (direction == new Vector3(0, 0, 1))
+        {
+            return BackSide;
+        }
+        else
+        {
+            return new Vector3();
+        }
+    }
+
+    private void SetLenternPosition(Face face)
+    {
+        if (face == Face.top || face == Face.right || face == Face.bottom)
+        {
+            Lentern_Active(SwitchPositionFromDirection(-Cube.transform.forward));
+        }
+        else
+        {
+            Lentern_Active(SwitchPositionFromDirection(Cube.transform.up));
+        }
+    }
+
+    private void SetSpawnPoint(CubePuzzleDataReader puzzleData)
+    {
+        _cubeCenter = puzzleData.BaseTransform.position;
+        float _cubeInterval = _cubeWidth + LenternInterval;
+
+        FrontSide = _cubeCenter + new Vector3(_cubeCenter.x, _cubeCenter.y, _cubeCenter.z + _cubeInterval);
+        BackSide = _cubeCenter + new Vector3(_cubeCenter.x, _cubeCenter.y, _cubeCenter.z - _cubeInterval);
+        LeftSide = _cubeCenter + new Vector3(_cubeCenter.x - _cubeInterval, _cubeCenter.y, _cubeCenter.z);
+        RightSide = _cubeCenter + new Vector3(_cubeCenter.x + _cubeInterval, _cubeCenter.y, _cubeCenter.z);
+    }
+
+    private IEnumerator MoveLantern(Vector3 spawnPoint)
+    {
+        float t = 0;
+        Vector3 current = transform.position;
+        Vector3 target = current - LanternMoveHight;
+        while (t < MoveTime)
+        {
+            t += Time.deltaTime;
+            transform.position = Vector3.Lerp(current, target, t);
+            yield return null;
+        }
+
+        this.transform.position = spawnPoint;
+
+        t = 0;
+        current = transform.position;
+        target = current + LanternMoveHight;
+        while (t < MoveTime)
+        {
+            t += Time.deltaTime;
+            transform.position = Vector3.Lerp(current, target, t);
+            yield return null;
+        }
+    }
+
+    public void SetMediator(IMediatorInstance mediator)
+    {
+
+    }
+    public void InstreamData(byte[] data)
+    {
+
+    }
+}
--- Assets/07. Prefabs/LevelPref/Scripts/Lantern_In.cs
+++ Assets/07. Prefabs/LevelPref/Scripts/Lantern_In.cs
@@ -2,7 +2,7 @@ using Puzzle;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class Lantern_In : MonoBehaviour, IInstance, IPuzzleInstance
+public class Lantern_In : MonoBehaviour, IInstance, IPuzzleInstance, IDestroyable
 {
     public DataReader DataReader { get; private set; } = new SystemReader();
 
@@ -22,54 +22,7 @@ public class Lantern_In : MonoBehaviour, IInstance, IPuzzleInstance
     private float _time;
     private Vector3 _cubeCenter;
     private byte _cubeWidth;
-    private bool _enterBossStage = false;
 
-    public void InstreamData(byte[] data)
-    {
-        if (data == SystemReader.CLEAR_TOP_FACE) // Ŭ����.
-        {
-            Lentern_Active(RightSide);
-        }
-        else
-        if (data == SystemReader.CLEAR_RIGHT_FACE)
-        {
-            Lentern_Active(RightSide);
-        }
-        else
-        if (data == SystemReader.CLEAR_BOTTOM_FACE)
-        {
-            Lentern_Active(FrontSide);
-        }
-        else
-        if (data == SystemReader.CLEAR_FRONT_FACE)
-        {
-            Lentern_Active(LeftSide);
-        }
-        else
-        if (data == SystemReader.CLEAR_LEFT_FACE)
-        {
-            Lentern_Active(BackSide);
-        }
-        else
-        if (data == SystemReader.CLEAR_BACK_FACE)
-        {
-            // Boss Todo
-            _enterBossStage = true;
-            SetSpawnPoint(_castingPuzzleData);
-        }
-        else
-        if (data == SystemReader.ROTATE_CUBE) // need => Rotation Data
-        {
-            IsDisapear = true;
-        }
-        else
-        if (data == null) // need => Fall Data
-        {
-            // Boss Todo
-            SetSpawnPoint(_castingPuzzleData);
-        }
-
-    }
 
     public void Init(CubePuzzleDataReader puzzleData)
     {
@@ -77,6 +30,11 @@ public class Lantern_In : MonoBehaviour, IInstance, IPuzzleInstance
         _castingPuzzleData.OnRotatedStage += SetLenternPosition;
         _cubeWidth = puzzleData.Width;
         SetSpawnPoint(_castingPuzzleData);
+    }
+
+    public void Destroy()
+    {
+        _castingPuzzleData.OnRotatedStage -= SetLenternPosition;
     }
 
     public void SetMediator(IMediatorInstance mediator)
@@ -126,7 +84,6 @@ public class Lantern_In : MonoBehaviour, IInstance, IPuzzleInstance
         }
     }
 
-
     private void Lentern_Active(Transform spwanPos)
     {
         this.gameObject.transform.position = spwanPos.position;
@@ -136,24 +93,13 @@ public class Lantern_In : MonoBehaviour, IInstance, IPuzzleInstance
         _time = 0;
     }
 
-    private void SetSpawnPoint(CubePuzzleDataReader puzzleData)
-    {
-        _cubeCenter = puzzleData.BaseTransform.position;
-        float _cubeInterval = _cubeWidth + LenternInterval;
-
-        FrontSide.position = _cubeCenter + new Vector3(_cubeCenter.x, _cubeCenter.y, _cubeCenter.z + _cubeInterval);
-        BackSide.position = _cubeCenter + new Vector3(_cubeCenter.x, _cubeCenter.y, _cubeCenter.z - _cubeInterval);
-        LeftSide.position = _cubeCenter + new Vector3(_cubeCenter.x - _cubeInterval, _cubeCenter.y, _cubeCenter.z);
-        RightSide.position = _cubeCenter + new Vector3(_cubeCenter.x + _cubeInterval, _cubeCenter.y, _cubeCenter.z);
-    }
-
     private Transform SwitchPositionFromDirection(Vector3 direction)
     {
-        if(direction == new Vector3(-1,0,0))
+        if (direction == new Vector3(-1, 0, 0))
         {
             return LeftSide;
         }
-        else if(direction == new Vector3(1, 0, 0))
+        else if (direction == new Vector3(1, 0, 0))
         {
             return RightSide;
         }
@@ -173,17 +119,80 @@ public class Lantern_In : MonoBehaviour, IInstance, IPuzzleInstance
 
     private void SetLenternPosition(Face face)
     {
-        if (_enterBossStage)
+        if (face == Face.top || face == Face.right || face == Face.bottom)
         {
-            if(face == Face.top || face == Face.right || face == Face.bottom)
-            {
-                Lentern_Active(SwitchPositionFromDirection(-this.transform.forward));
-            }
-            else
-            {
-                Lentern_Active(SwitchPositionFromDirection(this.transform.up));
-            }
-            
+            Lentern_Active(SwitchPositionFromDirection(-this.transform.forward));
+        }
+        else
+        {
+            Lentern_Active(SwitchPositionFromDirection(this.transform.up));
         }
     }
+
+    public void InstreamData(byte[] data)
+    {
+
+    }
+
+    private void SetSpawnPoint(CubePuzzleDataReader puzzleData)
+    {
+        _cubeCenter = puzzleData.BaseTransform.position;
+        float _cubeInterval = _cubeWidth + LenternInterval;
+
+        FrontSide.position = _cubeCenter + new Vector3(_cubeCenter.x, _cubeCenter.y, _cubeCenter.z + _cubeInterval);
+        BackSide.position = _cubeCenter + new Vector3(_cubeCenter.x, _cubeCenter.y, _cubeCenter.z - _cubeInterval);
+        LeftSide.position = _cubeCenter + new Vector3(_cubeCenter.x - _cubeInterval, _cubeCenter.y, _cubeCenter.z);
+        RightSide.position = _cubeCenter + new Vector3(_cubeCenter.x + _cubeInterval, _cubeCenter.y, _cubeCenter.z);
+    }
+
+#if false
+    public void InstreamData(byte[] data)
+    {
+        if (data == SystemReader.CLEAR_TOP_FACE) // Ŭ����.
+        {
+            Lentern_Active(RightSide);
+        }
+        else
+        if (data == SystemReader.CLEAR_RIGHT_FACE)
+        {
+            Lentern_Active(RightSide);
+        }
+        else
+        if (data == SystemReader.CLEAR_BOTTOM_FACE)
+        {
+            Lentern_Active(FrontSide);
+        }
+        else
+        if (data == SystemReader.CLEAR_FRONT_FACE)
+        {
+            Lentern_Active(LeftSide);
+        }
+        else
+        if (data == SystemReader.CLEAR_LEFT_FACE)
+        {
+            Lentern_Active(BackSide);
+        }
+        else
+        if (data == SystemReader.CLEAR_BACK_FACE)
+        {
+            // Boss Todo
+            _enterBossStage = true;
+            SetSpawnPoint(_castingPuzzleData);
+        }
+        else
+        if (data == SystemReader.ROTATE_CUBE) // need => Rotation Data
+        {
+            IsDisapear = true;
+        }
+        else
+        if (data == null) // need => Fall Data
+        {
+            // Boss Todo
+            SetSpawnPoint(_castingPuzzleData);
+        }
+
+    }
+    
+
+#endif
 }
